247a447faa603963ebb7c96bf56bb7f773aa49d2
Erick Staviasz <erickesc21@hotmail.com>
2023-05-08 16:00:48 -0300

    configuração do setup

---
+++ portifolio_react/portifolio/src/App.tsx
@@ -0,0 +1,13 @@
+import Test from './teste'
+import EstiloGlobal from './style'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Test />
+    </>
+  )
+}
+
+export default App
---
+++ portifolio_react/portifolio/src/teste.tsx
@@ -0,0 +1,5 @@
+function Test() {
+  return <div>ola</div>
+}
+
+export default Test
